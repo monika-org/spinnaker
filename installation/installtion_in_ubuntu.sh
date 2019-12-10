d046f8186fa12c918a157a83fb642a862cf3ed0d
monika rani <monika.rani@xebia.com>
2019-12-10 16:35:23 +0530

    adding the installation directory to install the spinnaker in 2 ways

---
+++ installation/installtion_in_ubuntu.sh
@@ -0,0 +1,39 @@
+SPINNAKER_VERSION=1.16.5
+curl -Os https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+sudo bash InstallHalyard.sh --user xeadmin
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker xeadmin
+sudo docker run -p 127.0.0.1:9090:9000 -d --name minio1 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio:RELEASE.2018-07-31T02-11-47Z server /data
+
+sudo apt-get -y install jq
+
+MINIO_SECRET_KEY=`echo $(sudo docker exec minio1 cat /root/.minio/config.json) |jq -r '.credential.secretKey'`
+MINIO_ACCESS_KEY=`echo $(sudo docker exec minio1 cat /root/.minio/config.json) |jq -r '.credential.accessKey'`
+echo $MINIO_SECRET_KEY | hal config storage s3 edit --endpoint http://127.0.0.1:9090 \
+    --access-key-id $MINIO_ACCESS_KEY \
+    --secret-access-key
+
+hal config storage edit --type s3
+
+# env flag that need to be set:
+
+
+set -e
+
+if [ -z "${SPINNAKER_VERSION}" ] ; then
+  echo "SPINNAKER_VERSION not set"
+  exit
+fi
+
+sudo hal config version edit --version $SPINNAKER_VERSION
+sudo hal deploy apply
+sudo echo "host: 0.0.0.0" |sudo tee \
+    /home/xeadmin/.hal/default/service-settings/gate.yml \
+    /home/xeadmin/.hal/default/service-settings/deck.yml
+sudo hal deploy apply
+sudo systemctl daemon-reload
+sudo hal deploy connect
+printf " -------------------------------------------------------------- \n|     Connect here to spinnaker: http://192.168.33.10:9000/    |\n --------------------------------------------------------------"
+sudo systemctl enable redis-server.service
+sudo systemctl start redis-server.service
---
+++ installation/spinnaker_installation_in_k8_cluster.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo "Installing Kubectl"
+
+kubectl version
+
+echo $?
+
+if [ $? -ne 0 ]
+then
+
+    echo "Kubectl not installed"
+    echo "Installing Kubectl using CURL:"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+    echo "Make the kubectl binary executable:"
+    chmod +x ./kubectl
+    echo "Move the binary in to your PATH:"
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    echo "Test to ensure the version you installed is up-to-date:"
+    kubectl version
+
+else 
+	    echo "Kubectl is already installed"
+
+fi
+
+echo "Installing Minikube"
+minikube start 
+echo $?
+if [ $? -ne 0 ]
+then
+	curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+      && chmod +x minikube
+    sudo mkdir -p /usr/local/bin/
+    sudo install minikube /usr/local/bin/
+    minikube start --memory=10000 --cpus=2 --kubernetes-version=v1.15.3
+
+else 
+   
+   echo "Minikube Already installed"
+
+fi 
+
+helm version
+if [ $? -ne 0 ]
+then
+    echo "Installing Helm"
+    curl -L https://git.io/get_helm.sh | bash -s -- --version v2.14.1
+    helm init 
+else
+    echo "Helm is already installed" 
+
+fi       
+
+
+helm install stable/spinnaker --name spinnaker --namespace=spinnaker
+echo "port-forwarding"
+export DECK_POD=$(kubectl get pods --namespace spinnaker -l "cluster=spin-deck" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace spinnaker $DECK_POD 9000 &
+export GATE_POD=$(kubectl get pods --namespace spinnaker -l "cluster=spin-gate" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace spinnaker $GATE_POD 8084 &
+echo "Entered into Helm"
+kubectl exec --namespace spinnaker -it spinnaker-spinnaker-halyard-0 bash \
+hal config features edit --artifacts true  \
+hal config artifact github enable \
+GITHUB_ACCOUNT_NAME=github_user \
+hal config artifact github account add ${GITHUB_ACCOUNT_NAME} \
+  --token \
+hal deploy apply
+
+
